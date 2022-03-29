eae650f9a7e9037b57c7561e05661343d087bed9
AlexHostetter2020 <61090238+AlexHostetter2020@users.noreply.github.com>
2022-03-29 14:29:26 -0400

    Add files via upload

---
+++ Pong.py
@@ -0,0 +1,16 @@
+# Pong game in python 3!
+
+# Import statements
+import turtle
+
+
+# Setting up the window
+window = turtle.Screen()
+window.title("Pong game by Alex:")
+window.bgcolor("black")
+
+
+
+
+
+
