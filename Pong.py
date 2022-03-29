fbcd92295b5e55b708e3d49cb84f93cd3f786bbd
AlexHostetter2020 <61090238+AlexHostetter2020@users.noreply.github.com>
2022-03-29 16:01:39 -0400

    Update Pong.py

--- Pong.py
+++ Pong.py
@@ -1,6 +1,7 @@
 # Pong game in python 3!
 
 # Import statements
+from tkinter import font
 import turtle
 
 
@@ -43,9 +44,25 @@ player_2.goto(350, 0)
 ball = turtle.Turtle()
 ball.speed(0)
 ball.shape("square")
-ball.color("white")
+ball.color("green")
 ball.penup()
 ball.goto(0, 0)
+ball.dx = 0.08
+ball.dy = 0.08
+
+# Score
+score_1 = 0
+score_2 = 0
+
+
+# Score turtle (pen)
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("pink")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player 1: 0      Player 2: 0", align="center", font=("mono", 24, "normal"))
 
 
 # Function definition for players
@@ -82,6 +99,45 @@ window.onkeypress(player_2_down, "Down")
 while True:
     window.update()
 
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
 
+    # Check if ball hits border
+    # Top and bottom border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
 
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
 
+    # Left and right border checking
+    if ball.xcor() > 390:
+        ball.goto(0,0)
+        # Switches direction of dx
+        ball.dx *= -1
+        # Updates score
+        score_1 += 1
+        pen.clear()
+        pen.write("Player 1: {}      Player 2: {}".format(score_1, score_2), align="center", font=("mono", 24, "normal"))
+
+    if ball.xcor() < -390:
+        ball.goto(0,0)
+        ball.dx *= -1
+        # Updates score
+        score_2 += 1
+        pen.clear()
+        pen.write("Player 1: {}      Player 2: {}".format(score_1, score_2), align="center", font=("mono", 24, "normal"))
+
+    # Player and ball collisions
+    if ball.xcor() > 340 and ball.xcor() < 350 and (ball.ycor() < player_2.ycor() + 40 and ball.ycor() > player_2.ycor() - 40):
+        ball.setx(340)
+        ball.dx *= -1
+
+    if ball.xcor() < -340 and ball.xcor() > -350 and (ball.ycor() < player_1.ycor() + 40 and ball.ycor() > player_1.ycor() - 40):
+        ball.setx(-340)
+        ball.dx *= -1
+
+# Done!!!
